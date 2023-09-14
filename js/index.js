4ee2c3cfac02ae380ba634cff9bddd6deec34396
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2023-09-14 22:23:05 +0300

    hw-js-24

---
+++ js/index.js
@@ -0,0 +1,18 @@
+                                                        // ex 1
+const button = document.querySelector(".btn")
+button.textContent = 1221
+const text = document.querySelector(".text")
+text.textContent = 321
+                                                        // ex 2
+const title = document.querySelector(".title")
+title.textContent = 111
+const img = document.querySelector(".img")
+img.src = "./img/12.jpg"
+                                                        // ex 3
+const link = document.querySelector(".link")
+link.setAttribute("href", "https://edu.goiteens.com/uk/learn/17138817/15266038/15266398/homework")
+const img2 = document.querySelector(".img2")
+img2.setAttribute("alt", "galaxy")
+                                                        // ex 4
+const list = document.querySelector(".list")
+list.firstElementChild.textContent = "1"
